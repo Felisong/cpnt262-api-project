79035fb6bc3f3671cf7f31246d8e53337b857ba1
Felisong <carolinahs100@gmail.com>
2024-11-11 19:26:03 -0700

    added @media queries and will adjust it on next push

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // API call
 `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}`;
 // API key
-const apiKey =`;
 
 // https://openweathermap.org/api/geocoding-api
 // get this api working as well so i can enter the city and it gets the cordinates for the weathermap
