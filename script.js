2858bfd8a99f9c2199f67cbacfb001eae6ecc7ea
Felisong <carolinahs100@gmail.com>
2024-11-12 16:43:21 -0700

    got the data successfully fetching it, so I am recieving it

--- script.js
+++ script.js
@@ -16,51 +16,65 @@ const apiKey = `dc20084f2a7551ca41da945c1298f0c7`;
 const weatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 const geoApi = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
-let masterData;
-
 // GETTING AND PLACING DATA FROM API
 document.addEventListener("DOMContentLoaded", async function getData() {
-  // variable for incoming data
-  // try to get data, catch if it doesn't load
-  // inside the data if it loads, can stylize or add in here
-  // catch
-  // const fetch data async () = > copy and paste from temi,
-  //should be a way to load the global and geo api into their respective variables for us later
+  let weatherData;
+  let geoData;
+
+  try {
+    // fetching api data
+    weatherData = await fetchData(weatherApi);
+    geoData = await fetchData(geoApi);
+    console.log(geoData);
+
+    // error in case api is empty
+    if (weatherData === 0 || geoData === 0) {
+      weatherList.innerHTML = `<p> unable to fetch data. Please try again later</p>`;
+    }
+
+    // inside the data if it loads, can stylize or add in here
+    // how do i want to start the actual work
+    //is user name in cookies?
+    //if not, alert here as soon as user loads in "Please input your name".
+    // save name input into cookies
+
+    // if so, load name from cookie.
+    // make user name appear in header.
+
+    // is there local storage of the last city/ temperature input?
+
+    // if so load the page with all inputs. Final step.
+
+    // if not, input city in form
+
+    // is the temperature celsius?
+
+    // if not go to fahrenheit
+
+    // if so you may submit preferences
+    // save inputs in local storage.
+
+    // is the temperature fahrenheit?
+
+    // if not- invalid. cannot be submitted
+
+    // if so, you may submit preferences
+    // save inputs in local storage.
+
+    // apply data to page.
+
+    // depending on keywords, interactive background if I have time.
+  } catch (error) {
+    console.log(`Error fetching data` > error);
+  }
 });
-
-// how do i want to start the actual work
-
-//is user name in cookies?
-//if not, alert here as soon as user loads in "Please input your name".
-// save name input into cookies
-
-// if so, load name from cookie.
-// make user name appear in header.
-
-// is there local storage of the last city/ temperature input?
-
-// if so load the page with all inputs. Final step.
-
-// if not, input city in form
-
-// is the temperature celsius?
-
-// if not go to fahrenheit
-
-// if so you may submit preferences
-// save inputs in local storage.
-
-// is the temperature fahrenheit?
-
-// if not- invalid. cannot be submitted
-
-// if so, you may submit preferences
-// save inputs in local storage.
-
-// apply data to page.
-
-// depending on keywords, interactive background if I have time.
 
 //FUNCTIONS BELOW
 
 // fetch API
+const fetchData = async (url) => {
+  const response = await fetch(url);
+  console.log("response:", response);
+  const data = await response.json();
+  return data.data;
+};
