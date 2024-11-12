1536e7cdfb0fb3c9c5e2c4bb29fd34d0e55544b2
Felisong <carolinahs100@gmail.com>
2024-11-11 19:18:52 -0700

    added some flexboxes and changed baackground color

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // API call
 `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}`;
 // API key
-const apiKey = `dc20084f2a7551ca41da945c1298f0c7`;
+const apiKey =`;
 
 // https://openweathermap.org/api/geocoding-api
 // get this api working as well so i can enter the city and it gets the cordinates for the weathermap
